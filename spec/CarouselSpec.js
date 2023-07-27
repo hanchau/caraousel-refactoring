b9157499975f6f01ff6c27d4d3aedd944a626de2
Anuj Chauhan <anujc@Anujs-MacBook-Air.local>
2023-07-27 13:21:34 +0530

    Updated the Scaffolding Tests

--- spec/CarouselSpec.js
+++ spec/CarouselSpec.js
@@ -21,32 +21,92 @@ describe("Photo Carousel", function () {
     });
 
     it( "given the data size to be nine the selected index should be 4.", function () {
+
+        // Act
         const coverFlow = new CoverFlow('#cover-flow', data);
+
+        // Assert
         expect(coverFlow.selectedIdx).toBe(4);
     });
 
-    it('selected cover should be 200px width and height.', function () {
+    it('selected cover should be 200px width and height and info box should be updated.', function () {
+
+        // Act
         const coverFlow = new CoverFlow('#cover-flow', data);
         expect(container.querySelector(`#IMAGE_${coverFlow.selectedIdx}`).style.width).toBe('200px');
         expect(container.querySelector(`#IMAGE_${coverFlow.selectedIdx}`).style.height).toBe('200px');
+
+        // Assert
+        expect($("#infoBox").text()).toEqual(data[coverFlow.selectedIdx].info);
     });
 
-    it('selected index should should be moved to `selectedIdx - 1` when clicked on backward button.', function () {
+    it('selected index should should be moved to `selectedIdx - 1` when clicked on backward button ' +
+        'and info box should be updated and should throw an error if the index is less than 0.', function () {
+        // Arrange
         const coverFlow = new CoverFlow('#cover-flow', data);
-        const oldSelectedIndex = coverFlow.selectedIdx;
-        $("#backward").click();
-        expect(coverFlow.selectedIdx).toBe(oldSelectedIndex - 1);
-        expect(container.querySelector(`#IMAGE_${oldSelectedIndex}`).style.width).toBe('100px');
-        expect(container.querySelector(`#IMAGE_${oldSelectedIndex}`).style.height).toBe('100px');
+
+        for (let index = data.length/2; index >= 0 ; index--) {
+            const oldSelectedIndex = coverFlow.selectedIdx;
+            if (index == 0) {
+                expect($("#backward").click()).toThrow(`You can't go farther left.`)}
+                break;
+            // Act
+            $("#backward").click();
+
+            // Assert
+            expect(coverFlow.selectedIdx).toBe(oldSelectedIndex - 1);
+            expect(container.querySelector(`#IMAGE_${oldSelectedIndex}`).style.width).toBe('100px');
+            expect(container.querySelector(`#IMAGE_${oldSelectedIndex}`).style.height).toBe('100px');
+            expect($("#infoBox").text()).toEqual(data[coverFlow.selectedIdx].info);
+        }
+
     });
 
-    it('selected index should should be moved to `selectedIdx + 1` when clicked on forward button.', function () {
+
+    it('selected index should should be moved to `selectedIdx + 1` when clicked on forward button ' +
+        'and info box should be updated and should throw an error if the index is greater than the length of the data.', function () {
+        // Arrange
         const coverFlow = new CoverFlow('#cover-flow', data);
-        const oldSelectedIndex = coverFlow.selectedIdx;
-        $("#forward").click();
-        expect(coverFlow.selectedIdx).toBe(oldSelectedIndex + 1);
-        expect(container.querySelector(`#IMAGE_${oldSelectedIndex}`).style.width).toBe('100px');
-        expect(container.querySelector(`#IMAGE_${oldSelectedIndex}`).style.height).toBe('100px');
+        for (let index = data.length/2; index <= data.length - 1 ; index++) {
+            const oldSelectedIndex = coverFlow.selectedIdx;
+
+            // Act
+            $("#forward").click();
+
+            // Assert
+            expect(coverFlow.selectedIdx).toBe(oldSelectedIndex + 1);
+            expect(container.querySelector(`#IMAGE_${oldSelectedIndex}`).style.width).toBe('100px');
+            expect(container.querySelector(`#IMAGE_${oldSelectedIndex}`).style.height).toBe('100px');
+            expect($("#infoBox").text()).toEqual(data[coverFlow.selectedIdx].info);
+        }
+    });
+
+
+    it('if clicked on `Beatles` cover the selectedIdx should be updated ' +
+        'and Beatles Covers height and width should be updated to 200px and 200px ' +
+        'and info box should be updated with the info `Beatles` ' +
+        'and previously selected image height and width should be reduced to 100px and 100px.', function () {
+        // Arrange
+        const coverFlow = new CoverFlow('#cover-flow', data);
+        let searchInfo = 'Beatles';
+        let indexOfBeatles = data.findIndex(function(item) {
+            return item.info === searchInfo;
+        });
+        let oldSelectedIdx = coverFlow.selectedIdx;
+        var imageofBeatles = `#IMAGE_${indexOfBeatles}`
+        var oldImage = `#IMAGE_${oldSelectedIdx}`
+
+        // Act
+        $(imageofBeatles).click();
+
+        // Assert
+        expect(coverFlow.selectedIdx).toBe(String(indexOfBeatles));
+        expect(container.querySelector(imageofBeatles).style.width).toBe('200px');
+        expect(container.querySelector(imageofBeatles).style.height).toBe('200px');
+        expect($("#infoBox").text()).toEqual(data[coverFlow.selectedIdx].info);
+        expect(container.querySelector(oldImage).style.height).toBe('100px');
+        expect(container.querySelector(oldImage).style.height).toBe('100px');
+
     });
 
 });
